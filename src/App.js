876b2493f71943c72faf3215fc908ed406578e93
eunbin55 <eunbin620@naver.com>
2022-08-31 22:00:39 +0900

    컴포넌트 반복(key) 및 배열 변형(배열 내장 함수 concat, filter)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React, { Component } from 'react';
-import ScrollBox from './ScrollBox';
+import IterationSample from './IterationSample';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <ScrollBox ref={(ref) => {this.scrollBox=ref}}/>
-        {/* 컴포넌트가 처음 렌더링될 때 this.scrollBox 값이 undefined이므로 
-        this.scrollBox.scrollToBottom 값을 읽어 오는 과정에서 오류가 발생한다. 
-        따라서, 화살표 함수 문법을 사용하여 새로운 함수를 만들고
-        그 내부에서 this.scrollBox.scrollToBottom 메서드를 실행하면, 
-        버튼을 누를 때 this.scrollBox.scrollToBottom 값을 읽어 와서 실행하므로 오류가 발생하지 않는다.
-        */}
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
-      </div>
-      );
+      <IterationSample/>
+    );
   }
 }
 
